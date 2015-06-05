6b0b3ae71a105c5259f2fb4721f44a536c0c0e7c
AmarPanjeta <apanjeta1@etf.unsa.ba>
2015-06-05 21:06:41 +0200

    WP8

---
+++ MobilnaAplikacija/MobilnaAplikacija/Page1.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+
+namespace MobilnaAplikacija
+{
+    public partial class Page1 : PhoneApplicationPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            using (NasaBazaContext db = new NasaBazaContext(NasaBazaContext.ConnectionString))
+            {
+                db.CreateIfNotExists();
+                try
+                {
+                    Table<NasaTabelicaAtrakcija> atrakcije = db.GetTable<NasaTabelicaAtrakcija>();
+                    var atrakcijeQuery = from a in atrakcije.ToList() select a;
+                    foreach (var atrakcija in atrakcijeQuery)
+                    {
+                        PivotItem p = new PivotItem();
+                        AtrakcijaKontrola atrakcijaKontrola = new AtrakcijaKontrola();
+                        atrakcijaKontrola.Naziv.Text =atrakcija.Naziv;
+                        atrakcijaKontrola.Opis.Text = atrakcija.OpisAtrakcije;
+                        atrakcijaKontrola.Kapacitet.Text = "Kapacitet: "+atrakcija.Kapacitet.ToString();
+                        atrakcijaKontrola.Ocjena.Text = "Ocjena: "+atrakcija.Ocjena.ToString();
+                        atrakcijaKontrola.Tip.Text = "Tip: "+atrakcija.TipAtrakcije;
+                        if (atrakcija.Slika.ToArray() != null && atrakcija.Slika.ToArray() is Byte[])
+                        {
+                            MemoryStream stream = new MemoryStream(atrakcija.Slika.ToArray());
+                            BitmapImage image = new BitmapImage();
+                            image.SetSource(stream);
+                            atrakcijaKontrola.Slika.Source = image;
+                        }
+                        p.Header = "Atrakcija"; //jelo.Ime;
+                        p.Content = atrakcijaKontrola;
+                        p.Width = 578;
+                        mojPivot.Items.Add(p);
+                    }
+
+                }
+                catch (Exception et)
+                {
+
+                }
+            }
+        }
+
+        private void mojPivot_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MobilnaAplikacija/MobilnaAplikacija/Page2.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace MobilnaAplikacija
+{
+    public partial class Page2 : PhoneApplicationPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+            tekst.Text = "Zabavni park je otvoren 1995.godine, a izgradila ga je cuvena filmska kompanija Universal Studios" + ". To se moze vidjeti i po velikoj statui posvecenoj Miki Mausu, kao" +
+"i po Souvenir shopu koji se nalazi unutar parka." +
+"Za detaljan obilazak ovog parka potrebno je nekoliko dana!" +
+"Park je podijeljen uu 5 tematskih cjelina, tj. 5 dijelova svijeta:" +
+"Mediteran, Polinezija, Kina, Meksiko i Divlji Zapad. " +
+"Svaki od ovih delova je prica za sebe i svaki je uradjen sa dosta detalja sa namjerom da" +
+"posjetioce na veoma uverljiv nacin vodi kroz neke daleke zemlje i predjele." +
+"U ovom parku cete pronaci najbrze i najstrasnije roller coastere na svijetu," +
+"ali se usput mozete osvjeziti i rashladiti na nekoj od nasih mnogobrojnih vodenih atrakcija." +
+"Kada je rijec o sigurnosti i nesrecama u ovom zabavnom parku, do sada nije zabiljezen nijedan takav slucaj." +
+"Ovaj zabavni park moze se pohvaliti visokom stopom sigurnosti i pouzdanosti svojih atrakcija," +
+"tako da je veca vjerovatnoca da ce vam se desiti nesto na putu do parka, nego na nekoj od zabavnih atrakcija parka." +
+"Osim sto je ovdje zavidno visoka stopa sigurnosti, isti epitet se moze dodijeliti i stopi adrenalina i zabave" +
+"koju ovaj park pruza! Posjetite nas i dozivite cari ovog parka koje je prosto nemoguce opisati rijecima!";
+        }
+
+        private void naklik(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
